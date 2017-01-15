f08359f51df89153476aae54170283abe7883104
Daniel Cordero (oak) <oak@0xdc.host>
2017-01-15 15:32:45 +0000

    Multiple overlay directory support
    Allow passing multiple root directories to be copied into the image's
    root partition. This could be used to separate the /boot contents for
    different devices.

--- .gfwrapper.sh
+++ .gfwrapper.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 if test "$#" = 0; then
-	echo "$0 <img> <stage3> <stage4/>" >&2
+	echo "$0 <img> <stage3> [<stage4/>...]" >&2
 	exit 1
 fi
 gf=$(which guestfish)
 
-(cd "$3"; tar c --owner=0 --group=0 .) | "$gf" \
-	-N "$1"=bootroot:vfat:ext4:1536M:256M:mbr -- \
+img=$1
+stage=$2
+shift 2
+
+for dir in $@; do
+	(cd "$dir"; tar c --owner=0 --group=0 .)
+done | "$gf" \
+	-N "$img"=bootroot:vfat:ext4:1536M:256M:mbr -- \
 	set-label /dev/sda1 BOOT : \
 	set-label /dev/sda2 ROOT : \
 	mount /dev/sda2 / : \
-	tar-in "$2" / compress:bzip2 : \
+	tar-in "$stage" / compress:bzip2 : \
 	mount /dev/sda1 /boot : \
 	-tar-in - / : \
 	umount /boot : umount / : \
