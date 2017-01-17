09193047b582ebcbff3eb17c897f32c85bd14ad2
Daniel Cordero (oak) <oak@0xdc.host>
2017-01-17 21:35:10 +0000

    ChromeOS device support
    Add support for the EFI boot method that ChromeOS devices utilise

---
+++ .gfomaha.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if test "$#" = 0; then
+	echo "$0 <img> <stage3> <stage4/> <device>" >&2
+	exit 1
+fi
+gf=$(which guestfish)
+
+(cd "$3"; tar c --owner=0 --group=0 .) | "$gf" \
+	-N "$1"=disk:2G -- \
+	part-init /dev/sda gpt : \
+	part-add /dev/sda p 34 16384 : \
+	part-add /dev/sda p 16385 32768   : \
+	part-add /dev/sda p 32769 65536 : \
+	part-add /dev/sda p 65537 4194270 : \
+	part-set-gpt-type /dev/sda 1 FE3A2A5D-4F32-41A7-B725-ACCC3285A309 : \
+	part-set-gpt-type /dev/sda 2 FE3A2A5D-4F32-41A7-B725-ACCC3285A309 : \
+	mkfs ext2 /dev/sda3 : \
+	mkfs ext4 /dev/sda4 : \
+	set-label /dev/sda3 BOOT : \
+	set-label /dev/sda4 ROOT : \
+	mount /dev/sda4 / : \
+	tar-in "$2" / compress:bzip2 : \
+	mount /dev/sda3 /boot : \
+	-tar-in - / : \
+	copy-file-to-device /boot/$4.itb /dev/sda1 : \
+	copy-file-to-device /boot/$4.itb /dev/sda2 : \
+	umount /boot : umount / : \
+	resize2fs-M /dev/sda4
+
+cgpt add -i 1 -T 15 -P 15 -S 1 $1
+cgpt add -i 2 -T 8  -P 8  -S 1 $1
+cgpt repair $1
--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 STAGE4 = $(shell find stage4/ -type f)
 VERSION ?= microserver
+BOARD ?= spring
 
 %.img.bz2: %.img
 	bzip2 -kfp $<
@@ -12,6 +13,9 @@ stage4/boot/%: %
 
 parallella.img: stage3-armv7a_hardfp-latest.tar.bz2 stage4/boot/parallella.bit.bin stage4/boot/uImage stage4/boot/devicetree.dtb $(STAGE4)
 	./.gfwrapper.sh $@ $< stage4 || rm -f $@
+
+omaha.img: stage3-armv7a_hardfp-latest.tar.bz2 stage4/boot/$(BOARD).itb $(STAGE4)
+	./.gfomaha.sh $@ $< stage4 || rm -f $@
 
 stage4/boot/parallella.bit.bin: parallella_e16_$(VERSION)_gpiose_7010.bit.bin
 	@test -d stage4/boot || mkdir -p stage4/boot
